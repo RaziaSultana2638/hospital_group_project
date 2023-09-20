a41e968f6e4e1b96527d218cb4d141baaf59717f
RaziaSultana2638 <raziasultana2638@gmail.com>
2023-09-20 10:49:37 +0600

    first commit

---
+++ resources/views/auth/login-new.blade.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="step-row">
+            <div id="progress"></div>
+            <div class="step-col"><small style="color: white;">HMS</small></div>
+
+        </div>
+        <form id="step1">
+            <h3>Sign In</h3>
+
+            <input type="text" name="username" placeholder="Username" required>
+
+            <input type="text" name="password" placeholder="Password" required>
+            <div class="btn-box">
+                <button type="submit" id="">Sign In</button>
+            </div>
+
+            <p>Don't have any account? Please <a href="/registration">Register</a></p>
+        </form>
+
+
+    </div>
+
+    <script src="./scripts.js"></script>
+</body>
+
+</html>
